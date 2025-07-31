bdc2d615aeeb1ed5cd2e34490835a16547bcc1c2
DAN.mx <jzlijgr2x@mozmail.com>
2025-07-31 01:03:27 -0600

    Mi primer commit

---
+++ configuration.py
@@ -0,0 +1,6 @@
+URL_SERVICE = "https://cnt-143d4e02-3de3-4a9b-a6cd-fddae607fd61.containerhub.tripleten-services.com/"
+DOC_PATH = "/docs/"
+LOG_MAIN_PATH = "/api/logs/main/"
+USERS_TABLE_PATH = "/api/db/resources/user_model.csv"
+CREATE_USER_PATH = "/api/v1/users/"
+PRODUCTS_KITS_PATH = "/api/v1/products/kits/"
